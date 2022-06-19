f0a62e8cba67054a92520c7aa2fbcabca11639f7
danieltrusca <truscadani@yahoo.com>
2022-06-19 16:17:25 +0300

    working on controllers for CRUD operations

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,22 +1,82 @@
-exports.getAllTasks = (req, res) => {
-  res.send("All tasks");
+const mongoose = require("mongoose");
+const Task = require("../models/Task");
+
+exports.getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
 };
 
-exports.createTask = (req, res) => {
-  //   res.send("Create a task");
-  res.json(req.body);
+exports.createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (err) {
+    res.status(500).json({ error: err.errors.name.message });
+  }
 };
 
-exports.getTask = (req, res) => {
-  //   res.send("get single task");
-  const {id} = req.params;
-  res.json({ taskId: id });
+exports.getTask = async (req, res) => {
+  const { id } = req.params;
+
+  // check if id is a valid mongoose ID
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send(`No task with id: ${id}`);
+
+  try {
+    const task = await Task.findById(id);
+    // const task = await Task.findOne({ _id: id })
+
+    if (!task) {
+      return res.status(404).json({ error: `No task with id : ${id}` });
+    }
+    res.status(200).json({ task });
+  } catch (err) {
+    res.status(500).json({ error: "There is not a task with this ID" });
+  }
 };
 
-exports.updateTask = (req, res) => {
-  res.send("update a task");
+exports.updateTask = async (req, res) => {
+  const { id } = req.params;
+
+  // check if id is a valid mongoose ID
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send(`No task with id: ${id}`);
+
+  try {
+    const task = await Task.findOneAndUpdate({ _id: id }, req.body, {
+      new: true,
+      runValidators: true
+    });
+    if (!task) {
+      return res.status(404).json({ error: `No task with id : ${id}` });
+    }
+
+    res.status(200).json({ task });
+  } catch (err) {
+    res.status(500).json({ error: "There is not a task with this ID" });
+  }
 };
 
-exports.deleteTask = (req, res) => {
-  res.send("delete a task");
+exports.deleteTask = async (req, res) => {
+  const { id } = req.params;
+
+  // check if id is a valid mongoose ID
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send(`No task with id: ${id}`);
+
+  try {
+    const task = await Task.findOneAndDelete({ _id: id });
+
+    if (!task) {
+      return res.status(404).json({ error: `No task with id : ${id}` });
+    }
+
+    res.status(200).json({ task });
+  } catch (err) {
+    res.status(500).json({ error: "There is not a task with this ID" });
+  }
 };
