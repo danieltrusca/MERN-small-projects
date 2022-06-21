c2c66071aa5ceddd1eda37fa8e67234c3dc24b5a
danieltrusca <truscadani@yahoo.com>
2022-06-21 17:22:01 +0300

    handling errors on server side

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const Task = require("../models/Task");
 
 const asyncWrapper = require("../middleware/async");
+const { createCustomError } = require("../errors/customError");
 
 exports.getAllTasks = asyncWrapper(async (req, res) => {
   const tasks = await Task.find({});
@@ -13,19 +14,20 @@ exports.createTask = asyncWrapper(async (req, res) => {
   res.status(201).json({ task });
 });
 
-exports.getTask = asyncWrapper(async (req, res) => {
+exports.getTask = asyncWrapper(async (req, res, next) => {
   const { id } = req.params;
 
   const task = await Task.findById(id);
   // const task = await Task.findOne({ _id: id })
 
   if (!task) {
-    return res.status(404).json({ error: `No task with id : ${id}` });
+    return next(createCustomError(`No task with id : ${id}`, 404));
+    // return res.status(404).json({ error: `No task with id : ${id}` });
   }
   res.status(200).json({ task });
 });
 
-exports.updateTask = asyncWrapper(async (req, res) => {
+exports.updateTask = asyncWrapper(async (req, res, next) => {
   const { id } = req.params;
 
   const task = await Task.findOneAndUpdate({ _id: id }, req.body, {
@@ -33,19 +35,19 @@ exports.updateTask = asyncWrapper(async (req, res) => {
     runValidators: true,
   });
   if (!task) {
-    return res.status(404).json({ error: `No task with id : ${id}` });
+    return next(createCustomError(`No task with id : ${id}`, 404));
   }
 
   res.status(200).json({ task });
 });
 
-exports.deleteTask = asyncWrapper(async (req, res) => {
+exports.deleteTask = asyncWrapper(async (req, res, next) => {
   const { id } = req.params;
 
   const task = await Task.findOneAndDelete({ _id: id });
 
   if (!task) {
-    return res.status(404).json({ error: `No task with id : ${id}` });
+    return next(createCustomError(`No task with id : ${id}`, 404));
   }
 
   res.status(200).json({ task });
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const connectDB = require("./db/connect");
 require("dotenv").config();
 const taskRoutes = require("./routes/tasks");
 const notFound=require("./middleware/NotFound");
-
+const errorHandlerMiddleware=require("./middleware/errorHandler");
 
 // middlewares
 app.use(express.json());
@@ -17,6 +17,7 @@ app.use(cors());
 
 app.use("/api/v1/tasks", taskRoutes);
 app.use(notFound);
+app.use(errorHandlerMiddleware);
 
 const PORT = process.env.PORT || 5000;
 
