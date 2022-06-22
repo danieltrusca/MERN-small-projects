0e64df1904453dbd3489b2f04b54f70ec7bb1a46
danieltrusca <truscadani@yahoo.com>
2022-06-22 15:13:57 +0300

    preparing for deployment

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app = express();
 const cors=require('cors');
 const connectDB = require("./db/connect");
 require("dotenv").config();
+const path=require("path");
+
 const taskRoutes = require("./routes/tasks");
 const notFound=require("./middleware/NotFound");
 const errorHandlerMiddleware=require("./middleware/errorHandler");
@@ -18,6 +20,22 @@ app.use(cors());
 app.use("/api/v1/tasks", taskRoutes);
 app.use(notFound);
 app.use(errorHandlerMiddleware);
+
+// --------------------------deployment------------------------------
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+// --------------------------deployment------------------------------
 
 const PORT = process.env.PORT || 5000;
 
