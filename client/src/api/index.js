3129c0dde6568718a7fdab9dfb2c40b97c452f6b
danieltrusca <truscadani@yahoo.com>
2022-06-22 15:30:59 +0300

    try to fix links

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/api/v1" });
+// const API = axios.create({ baseURL: "http://localhost:5000/api/v1" });
+const API = axios.create({
+  baseURL: "https://small-projects-mern.herokuapp.com//api/v1",
+});
 
 export const fetchTasks = () => API.get("/tasks");
 export const createTask = (newTask) => API.post("/tasks", newTask);
