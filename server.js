395aa9fb859f06f78918bd9ec0d4a7ac6cdcf595
danieltrusca <truscadani@yahoo.com>
2022-06-23 13:29:46 +0300

    prepare for development

--- server.js
+++ server.js
@@ -28,19 +28,7 @@ if (process.env.NODE_ENV == "production") {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
-// const __dirname = path.resolve();
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "/client/build")));
-
-//   app.get("*", (req, res) =>
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-//   );
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("API is running..");
-//   });
-// }
 // --------------------------deployment------------------------------
 
 app.use(notFound);
